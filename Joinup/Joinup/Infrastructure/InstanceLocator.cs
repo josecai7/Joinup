e8ebf8320c29a3d645485688bb37cd71d9f85eb0
Jose Antonio Soler Jimenez <jasoljim92@outlook.com>
2018-09-02 21:53:30 +0200

    Creada MainViewModel

---
+++ Joinup/Joinup/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,31 @@
+﻿using Joinup.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joinup.Infrastructure
+{
+    public class InstanceLocator
+    {
+        #region Properties
+
+        public MainViewModel Main 
+            {
+                get;
+                set; 
+            }
+
+        #endregion
+
+        #region Constructors
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+
+        #endregion
+    }
+}
---
+++ Joinup/Joinup/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joinup.ViewModels
+{
+    public class MainViewModel
+    {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+
+        #endregion
+    }
+}
